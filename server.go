c31c1e43206a02f356a42c2b5d8820a7fcc51c31
Oleg Vashkevich <ugluk@tut.by>
2024-01-03 09:57:02 +0300

    templ: установка и первые эксперименты

--- server.go
+++ server.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"net/http"
+	//"net/http"
+	"gothrix/components/common"
 
 	"github.com/labstack/echo/v4"
 )
@@ -9,7 +10,13 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return common.Hello("John").Render(c.Request().Context(), c.Response())
+		//return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		return common.Hello(id).Render(c.Request().Context(), c.Response())
+		//return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
